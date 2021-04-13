feaaf0475957748a198a1966d0a0306fa0ca46dc
NatsGt <nataliarodas7858@gmail.com>
2021-04-13 18:39:17 +0200

    created files for w2 databases

---
+++ databases/week2/Part3.sql
@@ -0,0 +1,27 @@
+use hyf_lesson2;
+
+-- 1
+select id, name, email
+from user
+WHERE email LIKE "%@spotify.com";
+
+-- 2
+SELECT task.title, user.name, status.name
+from user
+	join user_task on user.id=user_task.user_id
+	join task on task.id=user_task.task_id
+    join status on status.id = task.status_id
+WHERE status.name = 'Not started' and user.name = 'Donald Duck';
+
+-- 3
+SELECT task.title, user.name, task.created
+from user
+join user_task on user.id = user_task.user_id
+join task on task.id = user_task.task_id
+join status on task.status_id = status.id
+WHERE month(task.created) = 9 and user.name = 'Maryrose Meadows';
+
+-- 4
+select count(id), month(created)
+from task
+group by month(created);
