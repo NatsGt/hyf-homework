3d6619a2e4a55bf091b03e2b8bc54e144b615dfc
NatsGt <nataliarodas7858@gmail.com>
2021-05-10 16:40:57 +0200

    Made the changes to get meals

--- nodejs/week2/meal-sharing/src/backend/api/meals-router.js
+++ nodejs/week2/meal-sharing/src/backend/api/meals-router.js
@@ -4,40 +4,39 @@ const router = express.Router();
 const meals = require("./../data/meals.json");
 
 router.get("/", async (request, response) => {
-  let filteredMeals = meals;
-
-  if ("maxPrice" in request.query) {
-    const maxPrice = parseFloat(request.query.maxPrice);
-    if (isNaN(maxPrice)) {
-      response.status(400).send({ error: "Data type is not valid" })
-      return
-    } else {
-      filteredMeals = filteredMeals.filter(meal => meal.price < maxPrice);
-    }
-  }
-  if ("title" in request.query) {
-    const title = request.query.title;
-    filteredMeals = filteredMeals.filter(meal => meal.title.includes(title));
-  }
-  if ("createdAfter" in request.query) {
-    const createdAfter = Date.parse(request.query.createdAfter);
-    if (isNaN(createdAfter)) {
-      response.status(400).send({ error: "Data type is not valid" })
-      return
-    } else {
-      filteredMeals = filteredMeals.filter(meal => Date.parse(meal.createdAt) > createdAfter);
-    }
-  }
-  if ("limit" in request.query) {
-    const limit = request.query.limit;
-    if (isNaN(limit)) {
-      response.status(400).send({ error: "Data type is not valid" })
-      return
-    } else {
-      filteredMeals = filteredMeals.slice(0, limit);
-    }
-  }
   try {
+    let filteredMeals = meals;
+    if ("maxPrice" in request.query) {
+      const maxPrice = parseFloat(request.query.maxPrice);
+      if (isNaN(maxPrice)) {
+        response.status(400).send({ error: "Data type is not valid" })
+        return
+      } else {
+        filteredMeals = filteredMeals.filter(meal => meal.price < maxPrice);
+      }
+    }
+    if ("title" in request.query) {
+      const title = request.query.title;
+      filteredMeals = filteredMeals.filter(meal => meal.title.includes(title));
+    }
+    if ("createdAfter" in request.query) {
+      const createdAfter = Date.parse(request.query.createdAfter);
+      if (isNaN(createdAfter)) {
+        response.status(400).send({ error: "Data type is not valid" })
+        return
+      } else {
+        filteredMeals = filteredMeals.filter(meal => Date.parse(meal.createdAt) > createdAfter);
+      }
+    }
+    if ("limit" in request.query) {
+      const limit = request.query.limit;
+      if (isNaN(limit)) {
+        response.status(400).send({ error: "Data type is not valid" })
+        return
+      } else {
+        filteredMeals = filteredMeals.slice(0, limit);
+      }
+    }
     response.send(filteredMeals)
   } catch (error) {
     throw error;
