684e20e807e4a27913020d76759daab57d40ee54
NatsGt <nataliarodas7858@gmail.com>
2021-05-17 16:41:07 +0200

    Created components for todo list

---
+++ react/week1/todolist/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+
+const myTodo = [
+  {
+    id: 1,
+    task: "Finish my hyf homework",
+    dueDate: "Tue 18-05-2021"
+  },
+  {
+    id: 2,
+    task: "Shop groceries",
+    dueDate: "Wed 19-05-2021"
+  },
+  {
+    id: 3,
+    task: "Do laundry",
+    dueDate: "Thu 20-05-2021"
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div className="container">
+        <Table />
+      </div>
+    </div>
+  );
+}
+
+function Table() {
+  return (
+    <table className="todo-table">
+      <tr>
+        <th>Task</th>
+        <th>Due Date</th>
+      </tr>
+      {myTodo.map(list => {
+        return (
+          <Task key={list.id} task={list.task} dueDate={list.dueDate} />
+        )
+      }
+      )}
+
+    </table>
+  )
+}
+
+function Task(props) {
+  return (
+    <tr>
+      <td>{props.task}</td>
+      <td>{props.dueDate}</td>
+    </tr>
+  )
+}
+export default App;
