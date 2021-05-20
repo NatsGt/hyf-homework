0bc36707835f23a3c85d49c5ed978571b7f83a81
NatsGt <nataliarodas7858@gmail.com>
2021-05-20 13:22:29 +0200

    Checks if values passed are numbers

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -11,18 +11,23 @@ app.get("/calculator/:operator", (req, res) => {
     const calculatorOperator = req.params.operator;
     if (Object.keys(req.query).length > 0) {
         const digits = Object.values(req.query).flat().map(number => Number(number));
-        let total = 0;
-        if (calculatorOperator === "add") {
-            total = digits.reduce((acc, value) => acc + value)
-        } else if (calculatorOperator === "substract") {
-            total = digits.reduce((acc, value) => acc - value);
-        } else if (calculatorOperator === "multiply") {
-            total = digits.reduce((acc, value) => acc * value)
-        } else if (calculatorOperator === "divide") {
-            total = digits.reduce((acc, value) => acc / value);
+        const isDigitsNumbers = digits.every(number => !Number.isNaN(number))
+        if (isDigitsNumbers) {
+            let total = 0;
+            if (calculatorOperator === "add") {
+                total = digits.reduce((acc, value) => acc + value)
+            } else if (calculatorOperator === "subtract") {
+                total = digits.reduce((acc, value) => acc - value);
+            } else if (calculatorOperator === "multiply") {
+                total = digits.reduce((acc, value) => acc * value)
+            } else if (calculatorOperator === "divide") {
+                total = digits.reduce((acc, value) => acc / value);
+            }
+            res.send(total.toString())
+            return;
+        } else {
+            res.status(400).send({ error: 'All values must be numbers' })
         }
-        res.send(total.toString())
-        return;
     } else {
         res.send("0");
     }
@@ -32,18 +37,23 @@ app.post("/calculator/:operator", (req, res) => {
     const calculatorOperator = req.params.operator;
     if (Object.keys(req.body).length > 0) {
         const digits = Object.values(req.body).flat().map(number => Number(number));
-        let total = 0;
-        if (calculatorOperator === "add") {
-            total = digits.reduce((acc, value) => acc + value)
-        } else if (calculatorOperator === "substract") {
-            total = digits.reduce((acc, value) => acc - value);
-        } else if (calculatorOperator === "multiply") {
-            total = digits.reduce((acc, value) => acc * value)
-        } else if (calculatorOperator === "divide") {
-            total = digits.reduce((acc, value) => acc / value);
+        const isDigitsNumbers = digits.every(number => !Number.isNaN(number));
+        if (isDigitsNumbers) {
+            let total = 0;
+            if (calculatorOperator === "add") {
+                total = digits.reduce((acc, value) => acc + value)
+            } else if (calculatorOperator === "subtract") {
+                total = digits.reduce((acc, value) => acc - value);
+            } else if (calculatorOperator === "multiply") {
+                total = digits.reduce((acc, value) => acc * value)
+            } else if (calculatorOperator === "divide") {
+                total = digits.reduce((acc, value) => acc / value);
+            }
+            res.send(total.toString())
+            return;
+        } else {
+            res.status(400).send({ error: 'All values must be numbers' })
         }
-        res.send(total.toString())
-        return;
     } else {
         res.send("0");
     }
